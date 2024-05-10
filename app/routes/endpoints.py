7a7b98cc7c882c6990404f00862a15309da35eb7
David B Weinstein <davidbweinstein@gmail.com>
2024-05-10 10:57:11 -0400

    API and DB Work

--- app/routes/endpoints.py
+++ app/routes/endpoints.py
@@ -3,13 +3,13 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine
-from models.models_file import User, Base
+from models.models_file import User, Base, engine
 
 # Create the database engine
-engine = create_engine('sqlite:////database/app.db')
+# engine = create_engine('sqlite:////database/app.db')
 
 # Create the database tables (if they don't exist)
-Base.metadata.create_all(bind=engine)
+# Base.metadata.create_all(bind=engine)
 
 router = APIRouter()
 
