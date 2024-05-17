9fc96e7e122b65203112b7cb61ed8399bd3f559f
David B Weinstein <davidbweinstein@gmail.com>
2024-05-17 15:00:22 -0400

    deleted models_file.py which shoudl have been deleted in the last merge.  gonna merge this into main even though there are a couple extra things like Langchain in the requirments and the main_langchain.py with product dev map

---
+++ app/langchain/main_langchain.py
@@ -0,0 +1,32 @@
+# app/langchain/main_langchain.py
+
+
+# I want to build the following features usign langchiang
+
+
+# 1. General chat wiht Ollama
+#  -  in session
+
+
+# 2. Save conversation
+
+
+# 3. Load conversation
+
+
+# 4. upload documents
+#   -  allow user to upload documents like pdf
+#   -  save docs in regular db
+#   -  create docuemnt embeddings
+#   -  save docuemnt embeddings in vector db
+
+
+# 5.  RAG conversation with documents with Ollama
+
+# 6.  Impmlement other models like OpenAI
+
+# 7.  Site specific docuemnts in return from Ollama
+
+# 8.  Edit conversations (take out messeges that distract)
+
+# 9.  Fork conversations
