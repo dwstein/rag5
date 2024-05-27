b4775c9f38e0299856a30d4509573b69bda6d2ff
David B Weinstein <davidbweinstein@gmail.com>
2024-05-27 16:11:38 -0400

    added new front end container.  - the index.html file loads at http://localhost:3000/

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+// frontend/src/App.js
+
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to Your React App from App.js</h1>
+    </div>
+  );
+}
+
+export default App;
