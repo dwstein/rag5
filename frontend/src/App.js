0d4fd1f08824b2a45c9f8b9376a5bb8af122da68
David B Weinstein <davidbweinstein@gmail.com>
2024-05-29 22:34:50 -0400

    added bulma to App.js and added an container to the whole page.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 // frontend/src/App.js
 
 import React, { useEffect, useState } from "react";
+import 'bulma/css/bulma.min.css'; // Import Bulma CSS
 import HealthCheck from "./components/test-components/HealthCheck"; 
 import SafeMessages from "./components/test-components/SafeMessages"; // Import the new component
 import Login from "./components/nav-bar/login";
@@ -9,10 +10,12 @@ function App() {
 
 
   return (
-    <div>
-   
+    <div className="container">
       <Login /> {/* Use the Login component */}
-    </div>
+      {/* Add other components here */}
+      <HealthCheck />
+      <SafeMessages />
+  </div>
   );
 }
 
